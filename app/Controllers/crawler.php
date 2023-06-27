cddff097714089a18a265ab3d8e446b72bd65278
Nderi12 <nderikamau1212@gmail.com>
2023-06-27 22:47:51 +0300

    Addiing a reset fn for the web crawler

--- app/Controllers/crawler.php
+++ app/Controllers/crawler.php
@@ -51,6 +51,58 @@ class Crawler
 			echo $markup;
 		}
 		die();
+	}
 
+	/**
+	 * Using ajax to list links for admin
+	 *
+	 * @return void
+	 * @author Nderi Kamau <nderikamau1212@gmail.com>
+	 */
+	public static function view_links() {
+		$links = json_decode( get_transient( 'wpmedia_crawler_info' ), true );
+		if ( false !== $links && ! empty( $links ) ) {
+			$template_path = WP_MEDIA_CRAWLER_PLUGIN_DIR . 'resources/links-list.php';
+			include_once $template_path;
+		} else {
+			$markup = '<div class = \'no-links-msg\'>' . __( 'No links found.', 'wp-media-web-crawler' ) . '</div>';
+			echo $markup;
+		}
+		die();
+	}
+
+	/**
+	 * Ajax method to reset the crawler.
+	 *
+	 * @return void
+	 * @author Nderi Kamau <nderikamau1212@gmail.com>
+	 */
+	public static function reset_crawler() {
+		delete_transient( 'wpmedia_crawler_info' );
+		delete_option( 'wpmedia_crawler_started' );
+		wp_clear_scheduled_hook( 'crawler_task' );
+		// Delete sitemap.html file.
+		$sitemap_path = WP_MEDIA_CRAWLER_PLUGIN_DIR . '/storage/sitemap.html';
+		unlink( $sitemap_path );
+		$markup = '<div class = \'reset-msg\'>' . __( 'Resetting done. Click on Start Crawler to schedule the task again.', 'wp-media-web-crawler' ) . '</div>';
+		echo $markup;
+		die();
+	}
+
+	/**
+	 * Method to set cron schedules.
+	 *
+	 * @param array $schedules Schedules array.
+	 * @return schedules array
+	 * @author Nderi Kamau <nderikamau1212@gmail.com>
+	 */
+	public static function cron_schedules( $schedules ) {
+		if ( ! isset( $schedules['1hour'] ) ) {
+			$schedules['1hour'] = [
+				'interval' => 60 * 60,
+				'display'  => __( 'Once every 1 hour', 'wp-media-web-crawler' ),
+			];
+		}
+		return $schedules;
 	}
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,39 @@ jQuery("#start-crawler").click(function() {
 		},
 	}) 
 });
+
+jQuery("#view-links").click(function() {
+	jQuery("#links-section").html('');
+	jQuery(".loader").show();
+	jQuery.ajax({
+		type : "POST",
+		url : myAjax.ajaxurl,
+		data : {action: "view_links"},
+		success: function(response) {
+			jQuery(".loader").hide();
+			jQuery("#links-section").html(response);
+		},
+		error: function (response) {
+			jQuery(".loader").hide();
+		},
+	}) 
+});
+
+jQuery("#reset-crawler").click(function() {
+	jQuery("#links-section").html('');
+	jQuery(".loader").show();
+	jQuery.ajax({
+		type : "POST",
+		url : myAjax.ajaxurl,
+		data : {action: "reset_crawler"},
+		success: function(response) {
+			jQuery(".loader").hide();
+			jQuery("#links-section").html(response);
+			jQuery("#start-crawler").removeAttr("disabled");
+			jQuery("#view-links").hide();
+		},
+		error: function (response) {
+			jQuery(".loader").hide();
+		},
+	}) 
+});
