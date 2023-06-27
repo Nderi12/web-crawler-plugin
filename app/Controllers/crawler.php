9c5e5d95ae5f0bcd11fd15bae994ec85ee0160b1
Nderi12 <nderikamau1212@gmail.com>
2023-06-27 22:29:29 +0300

    Adding the crawler controller with all fns

---
+++ app/Controllers/crawler.php
@@ -0,0 +1,24 @@
+<?php
+/** Crawler file
+ *
+ * @package Controllers
+ * @author Nderi Kamau <nderikamau1212@gmail.com>
+ */
+
+namespace Controllers;
+
+/**
+ * A controller to handle all web crawler functions
+ * 
+ * @author Nderi Kamau <nderikamau1212@gmail.com>
+ */
+class Crawler
+{
+    /**
+     * Path to the class.
+     *
+     * @var $class_path
+     * @type string
+     */
+    protected static $class_path = WP_MEDIA_CRAWLER_NAMESPACE . '\Crawler';
+}
